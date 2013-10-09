c2bce833d8cf8a5f08888e2ba0054402c1e70493
Ondrej Sika <ondrej@ondrejsika.com>
2013-10-09 21:22:36 +0200

    update footer, movw css & js to separated files

---
+++ main.js
@@ -0,0 +1,80 @@
+var map = L.map('map').setView([49.7447656, 13.3752194], 3);
+
+getTiles = function(source){
+    return L.tileLayer(source, {}).addTo(map);
+}
+
+var tiles = getTiles('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+
+setMapSource = function(){
+    map.removeLayer(tiles);
+    tiles = getTiles(document.getElementById("mapSource").value)
+}
+
+updateGrid = function(){
+    grid.forEach(function(e){map.removeLayer(e)});
+    grid = [];
+    setGrid(Number(document.getElementById("grid").value))
+}
+
+var grid = [];
+setGrid = function(step){
+    for(i = 0 ; i <= 90; i=i+step){
+        line = L.polygon([
+            [0+i, -180],
+            [0+i, 180]
+        ], {weight:1}).addTo(map);
+        grid.push(line);
+        line = L.polygon([
+            [0-i, -180],
+            [0-i, 180]
+        ], {weight:1}).addTo(map);
+        grid.push(line);
+    }
+    for(i = 0; i <= 180; i=i+step){
+        line = L.polygon([
+            [-180, 0+i],
+            [180, 0+i]
+        ], {weight:1}).addTo(map);
+        grid.push(line);
+        line = L.polygon([
+            [-180, 0-i],
+            [180, 0-i]
+        ], {weight:1}).addTo(map);
+        grid.push(line);
+    }
+}
+
+var dotComma = ".";
+dotCommaSwitch = function(){
+    if (dotComma == ".") {
+        dotComma = ",";
+        getCoords();
+        return;
+    }
+    if (dotComma == ",") {
+        dotComma = ".";
+        getCoords();
+        return;
+    }
+}
+
+getCoords = function(){
+    document.getElementById("north").innerHTML = map.getBounds().getNorth().toString().replace(".", dotComma);
+    document.getElementById("south").innerHTML = map.getBounds().getSouth().toString().replace(".", dotComma);
+    document.getElementById("east").innerHTML = map.getBounds().getEast().toString().replace(".", dotComma);
+    document.getElementById("west").innerHTML = map.getBounds().getWest().toString().replace(".", dotComma);
+    document.getElementById("eastWest").innerHTML = (map.getBounds().getEast() - map.getBounds().getWest()).toString().replace(".", dotComma);
+    document.getElementById("northSouth").innerHTML = (map.getBounds().getNorth() - map.getBounds().getSouth()).toString().replace(".", dotComma);
+    document.getElementById("zoom").innerHTML = map.getZoom();
+}
+
+setMapWidthHeight = function(){
+    document.getElementById("map").style.width = document.getElementById("mapWidth").value;
+    document.getElementById("map").style.height = document.getElementById("mapHeight").value;
+}
+
+map.on('move', getCoords);
+getCoords();
+
+document.getElementsByClassName("leaflet-control-attribution")[0].style.display = "none";
